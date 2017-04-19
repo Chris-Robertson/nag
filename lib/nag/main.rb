ea6c25543a39422d7ff8754da2a943e1fd371116
Chris Robertson <chrisxrobertson@gmail.com>
2017-04-19 13:01:27 +1000

    Outputs to MacOS notification center every hour.

--- lib/nag/main.rb
+++ lib/nag/main.rb
@@ -11,8 +11,10 @@ class Nag
   def main
     repo_list.each do |r|
       repo = Rugged::Repository.new(r[:dir])
-      time_diff = (Time.now - repo.last_commit.time)/60/60/24
-      puts "It has been #{time_diff.round} days since your last commit to #{repo.workdir}"
+      time_diff = (Time.now - repo.last_commit.time) / 60 / 60 / 24
+      message = "It has been #{time_diff.round} days since your last commit to #{repo.workdir}"
+      # puts message
+      puts `echo #{message} | terminal-notifier -title Nag -sound glass -appIcon images/icon.png -contentImage image/icon.png`
     end
   end
 end
