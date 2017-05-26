34823ce56787f18de7623f05ca672643e7bf7588
Chris Robertson <chrisxrobertson@gmail.com>
2017-05-26 17:40:24 +1000

    Now only nags if it has been more than 7 days since last commit.

--- lib/nag/main.rb
+++ lib/nag/main.rb
@@ -1,6 +1,8 @@
 require 'rugged'
 require 'yaml'
 
+DAYS_TO_NAG_AFTER = 7
+
 class Nag
   attr_accessor :repo_list
   def initialize
@@ -11,10 +13,23 @@ class Nag
   def main
     repo_list.each do |r|
       repo = Rugged::Repository.new(r[:dir])
-      time_diff = (Time.now - repo.last_commit.time) / 60 / 60 / 24
-      message = "It has been #{time_diff.round} days since your last commit to #{repo.workdir}"
-      # puts message
-      puts `echo #{message} | terminal-notifier -title Nag -sound glass -appIcon images/icon.png -contentImage image/icon.png`
+      days_since = time_diff(repo.last_commit.time)
+
+      next if days_since < DAYS_TO_NAG_AFTER
+
+      message = "It has been #{days_since} days since your last commit
+                 to #{repo.workdir}"
+      puts `echo #{message} | terminal-notifier -title Nag
+                                                -sound glass
+                                                -appIcon images/icon.png
+                                                -contentImage image/icon.png`
+      sleep 3
     end
+  end
+
+  def time_diff(last_commit_time)
+    time_in_seconds = Time.now - last_commit_time
+    time_in_days = time_in_seconds / 60 / 60 / 24
+    time_in_days.round
   end
 end
